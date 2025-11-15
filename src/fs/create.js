817de8c20c832593591eefb04db3558c414999da
Mark <mark.tabletpc@gmail.com>
2025-11-15 19:35:25 +0100

    feat: "create" script

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,28 @@
+import { writeFile, access } from 'fs/promises';
+import { join } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
 const create = async () => {
-  // Write your code here
+    const __dirname = path.dirname(fileURLToPath(new URL(import.meta.url)));
+    const filePath = join(__dirname, 'files', 'fresh.txt');
+    let fileExists = true;
+
+    try {
+        await access(filePath);
+    } catch (err) {
+        fileExists = false;
+    }
+
+    if (fileExists) {
+        throw new Error('FS operation failed. File exists');
+    }
+
+    try {
+        await writeFile(filePath, 'I am fresh and young', 'utf-8');
+    } catch {
+        throw new Error('FS operation failed');
+    }
 };
 
 await create();
