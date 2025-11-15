e3890fd1249c04c6fa806a2628ae16977eb5bf5a
Mark <mark.tabletpc@gmail.com>
2025-11-15 20:25:26 +0100

    feat: "rename" script

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -2,28 +2,27 @@ import {cp, lstat} from 'fs/promises';
 import { join, dirname } from 'path';
 import { fileURLToPath } from 'url';
 
-
 const copy = async () => {
-        const __dirname = dirname(fileURLToPath(import.meta.url));
-        const srcFilePath = join(__dirname, 'files');
-        const destFilePath = join(__dirname, 'files_copy');
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const srcFilePath = join(__dirname, 'files');
+    const destFilePath = join(__dirname, 'files_copy');
 
-        const srcFolderExists = await lstat(srcFilePath).then(() => true).catch(() => false);
-        const destFolderExists = await lstat(destFilePath).then(() => true).catch(() => false);
+    const srcFolderExists = await lstat(srcFilePath).then(() => true).catch(() => false);
+    const destFolderExists = await lstat(destFilePath).then(() => true).catch(() => false);
 
-        if (!srcFolderExists) {
-            throw new Error('FS operation failed. Src folder does not exist');
-        }
+    if (!srcFolderExists) {
+        throw new Error('FS operation failed. Src folder does not exist');
+    }
 
-        if (destFolderExists) {
-            throw new Error('FS operation failed. Dest folder exists');
-        }
+    if (destFolderExists) {
+        throw new Error('FS operation failed. Dest folder exists');
+    }
 
-        try {
-            await cp(srcFilePath, destFilePath, { recursive: true });
-        } catch {
-            throw new Error('FS operation failed. Copying failed');
-        }
+    try {
+        await cp(srcFilePath, destFilePath, { recursive: true });
+    } catch {
+        throw new Error('FS operation failed. Copying failed');
+    }
 };
 
 await copy();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,29 @@
+import { access, rename as fsRename } from 'fs/promises';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 const rename = async () => {
-  // Write your code here
-};
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const srcFilePath = join(__dirname, 'files', 'wrongFilename.txt');
+    const destFilePath = join(__dirname, 'files', 'properFilename.md');
+
+    const destFileExists = await access(destFilePath).then(() => true).catch(() => false);
+
+    if (destFileExists) {
+        throw new Error('FS operation failed. Dest file already exists');
+    }
+
+    try {
+        await access(srcFilePath);
+    } catch (err) {
+        throw new Error('FS operation failed. Src file does not exist');
+    }
+
+    try {
+        await fsRename(srcFilePath, destFilePath);
+    } catch {
+        throw new Error('FS operation failed. Renaming failed');
+    }
+}
 
 await rename();
