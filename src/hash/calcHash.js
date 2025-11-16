c73b9dc5b980726c47406d496ed8a3ef1f189382
Mark <mark.tabletpc@gmail.com>
2025-11-16 10:04:06 +0100

    feat: implement "calcHash"

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
+import { createHash } from 'crypto';
+import { createReadStream } from 'fs';
+import {dirname, join} from "path";
+import {fileURLToPath} from "url";
+
 const calculateHash = async () => {
-  // Write your code here
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const srcFilePath = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
+    //V1
+    const hash = createHash('sha256');
+    const fileStream = createReadStream(srcFilePath);
+    fileStream.on('data', (chunk) => hash.update(chunk));
+    fileStream.on('end', () => console.log(hash.digest('hex')));
+    fileStream.on('error', (err) => console.error('Error reading file:', err));
+
+    //V2
+    const hash2 = createHash('sha256');
+    const fileStream2 = createReadStream(srcFilePath);
+    fileStream2.pipe(hash2).on('finish', () => console.log(hash2.digest('hex')));
 };
 
 await calculateHash();
