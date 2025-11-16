52a10d6d563863ecee499b7ff013232b65d177fe
Mark <mark.tabletpc@gmail.com>
2025-11-16 09:14:58 +0100

    feat: implementation "parseArgs"

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-  // Write your code here
+    const args = process.argv.slice(2);
+
+    const acc = args.reduce((acc, arg, index) => {
+        if (index % 2 === 0 && arg.startsWith('--')) {
+            const key = arg.slice(2);
+            const value = args[index + 1] || '';
+            acc.push(`${key} is ${value}`);
+        }
+        return acc;
+    }, [])
+
+    console.log(acc.join(', '));
 };
 
 parseArgs();
