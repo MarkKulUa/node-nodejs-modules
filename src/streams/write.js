adde636efe8e1b07e18a3c02057984624181eb54
Mark <mark.tabletpc@gmail.com>
2025-11-16 11:59:49 +0100

    feat: implement "WriteStream"

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,20 @@
-const write = async () => {
-  // Write your code here
+import { createWriteStream } from 'fs';
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const write = () => {
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const srcFilePath = join(__dirname, 'files', 'fileToWrite.txt');
+
+    // V1
+    const stream = createWriteStream(srcFilePath, 'utf-8');
+    process.stdin.on('data', (chunk) => stream.write(chunk));
+    process.stdin.on('end', () => stream.end());
+    stream.on('error', (err) => console.error('Error:', err));
+
+    // V2
+    // const stream2 = createWriteStream(srcFilePath, 'utf-8');
+    // process.stdin.pipe(stream2);
 };
 
 await write();
