736de955fe1306fa8e529534a08cc23f0f157f7d
Mark <mark.tabletpc@gmail.com>
2025-11-16 07:49:10 +0100

    feat: "list" script

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,30 @@
+import { access, readdir } from 'fs/promises';
+import { join, dirname } from 'path';
+import { fileURLToPath } from "url";
+import { FS_OPERATION_FAILED } from "./constants.js";
+
 const list = async () => {
-  // Write your code here
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const srcFilePath = join(__dirname, 'files');
+
+    const srcFolderExists = await access(srcFilePath).then(() => true).catch(() => false);
+
+    if (!srcFolderExists) {
+        throw new Error(FS_OPERATION_FAILED);
+    }
+
+    try {
+        const entries = await readdir(srcFilePath, { withFileTypes: true });
+
+        const files = entries
+            .filter(entry => entry.isFile())
+            .map(entry => entry.name);
+
+        console.log(files);
+
+    } catch {
+        throw new Error(FS_OPERATION_FAILED);
+    }
 };
 
 await list();
