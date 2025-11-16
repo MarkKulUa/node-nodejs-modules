522011e65ee4331ba56d0d5e3081a393bf96f94e
Mark <mark.tabletpc@gmail.com>
2025-11-16 08:00:38 +0100

    feat: "read" file script

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,24 @@
+import { access, readFile } from 'fs/promises';
+import { join, dirname } from 'path';
+import {fileURLToPath} from "url";
+import { FS_OPERATION_FAILED } from './constants.js';
+
 const read = async () => {
-  // Write your code here
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const srcFilePath = join(__dirname, 'files', 'fileToRead.txt');
+
+    const srcFileExists = await access(srcFilePath).then(() => true).catch(() => false);
+
+    if (!srcFileExists) {
+        throw new Error(FS_OPERATION_FAILED);
+    }
+
+    try {
+        const content = await readFile(srcFilePath, 'utf-8');
+        console.log(content);
+    } catch {
+        throw new Error(FS_OPERATION_FAILED);
+    }
 };
 
 await read();
