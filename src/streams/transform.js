a8de77ee9950584a23559bbd91cea771f9746b45
Mark <mark.tabletpc@gmail.com>
2025-11-16 13:29:42 +0100

    feat: implement "input transform"

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,21 @@
+import { Transform as TransformClass } from 'stream';
+import { pipeline } from 'stream/promises';
+
 const transform = async () => {
-  // Write your code here
+
+    const myTransform = new TransformClass({
+        transform(chunk, encoding, callback) {
+            chunk = chunk.toString().split('').reverse().join('');
+
+            callback(null, chunk);
+        }
+    });
+
+    await pipeline(
+        process.stdin,
+        myTransform,
+        process.stdout
+    );
 };
 
 await transform();
