3e3264562947f033e54d22a29de25de0b19cc994
Mark <mark.tabletpc@gmail.com>
2025-11-27 18:24:24 +0100

    feat: add 03-error-handling-async

---
+++ src/03-error-handling-async/index.test.ts
@@ -0,0 +1,30 @@
+import { throwError, throwCustomError, resolveValue, MyAwesomeError, rejectCustomError } from './index';
+
+describe('resolveValue', () => {
+  test('should resolve provided value', async () => {
+      const result = await resolveValue(42);
+      expect(result).toBe(42);
+  });
+});
+
+describe('throwError', () => {
+  test('should throw error with provided message', () => {
+      expect(() => throwError('error msg')).toThrow('error msg');
+  });
+
+  test('should throw error with default message if message is not provided', () => {
+      expect(() => throwError()).toThrow('Oops!');
+  });
+});
+
+describe('throwCustomError', () => {
+  test('should throw custom error', () => {
+      expect(() => throwCustomError()).toThrow('This is my awesome custom error!');
+  });
+});
+
+describe('rejectCustomError', () => {
+  test('should reject custom error', async () => {
+      await expect(rejectCustomError()).rejects.toThrow(MyAwesomeError);
+  });
+});
